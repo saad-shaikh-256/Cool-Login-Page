d334504da5ba529439892fb42e6f587db8933e48
Saad Shaikh <shaikhsaad256@gmail.com>
2025-01-10 22:31:52 +0530

    Login Page Also Completed

---
+++ JS/login.js
@@ -0,0 +1,54 @@
+gsap.to("#textBack",
+   {
+      y: "0%",
+      opacity: 1,
+      duration: 1.5,
+      ease: "power4.inOut",
+   }
+);
+
+gsap.to("#inputs",
+   {
+      y: "0%",
+      opacity: 1,
+      duration: 1.5,
+      ease: "power4.inOut",
+   }
+);
+
+gsap.to("#button",
+   {
+      y: "0%",
+      opacity: 1,
+      duration: 1.5,
+      ease: "power4.inOut",
+   }
+);
+
+gsap.to(".continue",
+   {
+      y: "0%",
+      opacity: 1,
+      duration: 1.5,
+      ease: "power4.inOut",
+   }
+);
+
+gsap.to("#socialMedia",
+   {
+      y: "0%",
+      opacity: 1,
+      duration: 1.5,
+      ease: "power4.inOut",
+   }
+);
+
+gsap.to(".signUp",
+   {
+      y: "0%",
+      opacity: 1,
+      duration: 1.5,
+      ease: "power4.inOut",
+   }
+);
+

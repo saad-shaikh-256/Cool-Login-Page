3f050afd2312a5be096c719a94addb22eef010b8
Saad Shaikh <shaikhsaad256@gmail.com>
2025-01-10 21:14:04 +0530

    Updated Code with Some GSAP

---
+++ index.js
@@ -0,0 +1,46 @@
+gsap.to("#skip",
+   {
+      y: "0%",
+      opacity: 1,
+      duration: 1.5,
+      ease: "power4.inOut",
+   }
+);
+
+
+gsap.to("#image",
+   {
+      y: "0%",
+      opacity: 1,
+      duration: 1.5,
+      ease: "power4.inOut",
+   }
+);
+
+
+gsap.to("#startupText",
+   {
+      y: "0%",
+      opacity: 1,
+      duration: 1.5,
+      ease: "power4.inOut",
+   }
+);
+
+gsap.to("#slider",
+   {
+      y: "0%",
+      opacity: 1,
+      duration: 1.5,
+      ease: "power4.inOut",
+   }
+);
+
+gsap.to("#button",
+   {
+      y: "0%",
+      opacity: 1,
+      duration: 1.5,
+      ease: "power4.inOut",
+   }
+);
